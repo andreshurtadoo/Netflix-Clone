beee320ed69caaec846a17ffe5f02a30ddb8d471
andreshurtadoo <hurtadoandres73@hotmail.com>
2021-09-07 11:30:15 -0400

    acomodando template

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const api = require('./router/routerMovies')
-// const api2 = require('./router/routerOne')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -9,8 +8,6 @@ app.set('views', __dirname + '/views')
 app.use(express.static(__dirname + '/public'))
 
 app.use('/', api)
-
-// app.use('/', api2)
 
 app.use((req, res, next) => {
     res.status(404).render('404',{
